f5dec48cdae797a0e38ec0294bede4fd478da6b9
mizolo <mizolo@taltech.ee>
2024-10-30 16:38:56 +0200

    first push

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/symed-project/",
-  esbuild: {
-    loader: 'jsx', // Ensures JSX is handled as JavaScript
-    include: /\.jsx?$/,
-  }
+  base: "/",
+  // esbuild: {
+  //   loader: 'jsx', // Ensures JSX is handled as JavaScript
+  //   include: /\.jsx?$/,
+  // }
 })
