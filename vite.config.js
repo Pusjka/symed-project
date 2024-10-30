689108c6c730fad1240c3d74e20ed1ddd1bf651f
mizolo <mizolo@taltech.ee>
2024-10-30 16:43:06 +0200

    first push

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  // esbuild: {
-  //   loader: 'jsx', // Ensures JSX is handled as JavaScript
-  //   include: /\.jsx?$/,
-  // }
+  esbuild: {
+    loader: 'jsx', // Ensures JSX is handled as JavaScript
+    include: /\.jsx?$/,
+  }
 })
