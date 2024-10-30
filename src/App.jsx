c568892fa18dc29db9f5527c184fa15decb02e58
mizolo <mizolo@taltech.ee>
2024-10-30 16:51:22 +0200

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import ProductDetails from "./Pages/ProductDetails.jsx";
 function App() {
 
     return (
-        <BrowserRouter basename={"/symed-project"}>
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Header/>}>
                     <Route index element={<Home />}/>
