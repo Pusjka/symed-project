28363123cbbda4d5b38cc86a16c969acd84a060a
mizolo <mizolo@taltech.ee>
2024-11-01 11:43:37 +0200

    first push

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import './index.scss'
 import React from 'react';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-const App = () => <h1>Hello, World!</h1>;
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
