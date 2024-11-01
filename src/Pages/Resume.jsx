1dbbafe7aa155d473db281853c5057ddf26d2355
mizolo <mizolo@taltech.ee>
2024-11-01 11:31:06 +0200

    first push

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,31 +1,11 @@
 import config from "../Config.js";
-import '/src/CSSFiles/Resume.scss'
 import {useRef, useState} from "react";
-import { usePdf } from "@mikecousins/react-pdf";
-import resume from "../Files/resume.pdf";
+import '/src/CSSFiles/Resume.scss'
 import {useTranslation} from "react-i18next";
 
 function Resume() {
     const { t, i18n } = useTranslation();
-    const [page, setPage] = useState(1);
-    const canvasRef = useRef(null);
-    const {pdfDocument, pdfPage} = usePdf({
-        file: resume,
-        page,
-        canvasRef,
-    });
-
-    const downloadPDF = () => {
-        fetch(resume).then(response => {
-            response.blob().then(blob => {
-                const fileURL = window.URL.createObjectURL(blob);
-                let alink = document.createElement('a');
-                alink.href = fileURL;
-                alink.download = 'Mihhail Zolotarjov - resume.pdf';
-                alink.click();
-            })
-        })
-    }
+    const [, setPage] = useState(1);
 
     return(
         <div className={"resumePage"}>
@@ -34,16 +14,10 @@ function Resume() {
                     <div className={"mainResumeText"}>{t("resumePageTitle")}</div>
                     <div className={"notMainResumeText"}>{t("resumePageDescription")}</div>
                 </div>
-                <div className={"resumeOnPage"}>
-                    {!pdfDocument && <span>Loading...</span>}
-                    <canvas ref={canvasRef} className={"resume"}/>
-                </div>
-                <div onClick={downloadPDF} className={"downloadResumeDiv"}>
-                    {t("resumePageDownload")}
-                    <span className="material-symbols-outlined downloadResumeButton">
-                        download
-                    </span>
-                </div>
+                {t("resumePageDownload")}
+                <span className="material-symbols-outlined downloadResumeButton">
+                    download
+                </span>
             </div>
         </div>
     )
