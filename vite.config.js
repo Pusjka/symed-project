7da47f6d128e5dc196f0929661c09c279f64b949
mizolo <mizolo@taltech.ee>
2024-11-07 14:23:01 +0200

    Updates

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+// import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   // base: "/",
-  build: {
-    target: 'esnext' // This will allow top-level await
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler' // or "modern"
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  // build: {
+  //   target: 'esnext' // This will allow top-level await
+  // },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       api: 'modern-compiler' // or "modern"
+  //     }
+  //   }
+  // },
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, './src')
+  //   }
+  // }
 });
